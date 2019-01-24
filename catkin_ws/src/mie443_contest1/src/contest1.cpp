608978cc7da32114ab0129c9f85b00688768e452
Matthew Chang <matt@Matthews-MacBook-Pro.local>
2019-01-24 10:10:41 -0500

    Last Update:Jan 24 10:10am by Matthew

--- catkin_ws/src/mie443_contest1/src/contest1.cpp
+++ catkin_ws/src/mie443_contest1/src/contest1.cpp
@@ -8,7 +8,20 @@
 #include <stdio.h>
 #include <cmath>
 
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
 using namespace std;
+
+//-----Odometry Variables-----//
+double posX, posY, yaw;
+double pi=3.1416;
+
+void odomCallback (const navmsgs::Odometry::ConstPtr& msg){
+    posx = msg->pose.pose.position.x;
+    posy = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+}
 
 //-----Bumper Variables-----//
 bool bumperLeft = 0;
@@ -16,13 +29,16 @@ bool bumperRight = 0;
 bool bumperCentre = 0;
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr msg){
-	if(msg.bumper == 0)
+	if(msg->bumper == 0)
 		bumperLeft = !bumperLeft;
-	else if(msg.bumper == 1)
+	else if(msg->bumper == 1)
 		bumperCentre = !bumperCentre;
-	else if(msg.bumper == 2)
+	else if(msg->bumper == 2)
 		bumperRight = !bumperRight;
 }
+
+//-----Laser Variables-----//
+
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	//fill with your code
@@ -33,7 +49,8 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "image_listener");
 	ros::NodeHandle nh;
 	teleController eStop;
-
+    
+    ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback);
 	ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
 	ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
 
@@ -50,6 +67,8 @@ int main(int argc, char **argv)
 		//...................................
 
 		//fill with your code
+        ROS_INFO(“Position: (%f, %f) Orientation: %f rad or %f degrees.”, posX, posY, yaw, yaw*180/pi);
+        ROS_INFO(“Laser Range: %i”, laserRange);
 
   		vel.angular.z = angular;
   		vel.linear.x = linear;
