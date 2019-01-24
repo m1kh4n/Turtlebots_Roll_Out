c1d20558111fb8e1e21454eff4ed493f7b4263b8
albertliu132 <albrt.liu@mail.utoronto.ca>
2019-01-24 13:23:18 -0500

    movement code added, untested

--- catkin_ws/src/mie443_contest1/src/contest1.cpp
+++ catkin_ws/src/mie443_contest1/src/contest1.cpp
@@ -22,7 +22,7 @@ void odomCallback (const nav_msgs::Odometry::ConstPtr& msg){
 	posY = msg->pose.pose.position.y;
 	yaw = tf::getYaw(msg->pose.pose.orientation);
 
-	ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180.0/pi);
+	//ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180.0/pi);
 }
 
 //-----Bumper Variables-----//
@@ -74,6 +74,51 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	//ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);
 }
 
+//-----Movement Functions-----//
+double angular = 0.0;
+double angleSpeed=pi/12;
+
+double linear = 0.0;
+double maxSpeed = 0.25;
+int safety = 1;
+
+void moveForward(float speed, int safety){
+	angular = 0;
+	if (safety == 0){
+		if(speed < maxSpeed)
+			linear = speed;
+		else
+			linear = maxSpeed;
+	}
+	else if (safety == 1){
+		if (laserRange == 0)
+			linear = 0;
+		if (laserRange < 1)
+			linear = 0.1;
+		else if(speed < maxSpeed)
+			linear = speed;
+		else
+			linear = maxSpeed;
+	}
+	ROS_INFO("Robot moving forward with speed of: %lf.", linear); 
+}
+
+void rotate(float angle){
+	linear = 0;
+	double starting_yaw = yaw;
+	if (angle > 0){
+		while (yaw-starting_yaw < angle){
+			angular = angleSpeed;	
+		}
+	}
+	else if (angle < 0){
+		while (yaw-starting_yaw > angle){
+			angular = -angleSpeed;	
+		}
+	}
+	ROS_INFO("Robot turning with speed of: %lf.", angular);
+} 
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "image_listener");
@@ -86,8 +131,7 @@ int main(int argc, char **argv)
 
 	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
-	double angular = 0.0;
-	double linear = 0.0;
+	
 	geometry_msgs::Twist vel;
 
 	while(ros::ok()){
@@ -95,8 +139,8 @@ int main(int argc, char **argv)
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
 		//...................................
-
-		//fill with your code
+		
+		moveForward(1.0, safety);
 	        //ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180/pi);
         	ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);
 		//ROS_INFO("Laser Range: %i", laserRange);
