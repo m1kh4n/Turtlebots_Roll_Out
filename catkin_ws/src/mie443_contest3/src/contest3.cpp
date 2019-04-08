5492ac09b7a91a8840205ea0ebb4bce0c0c97101
Albert Liu <albertliu@Alberts-MacBook-Pro.local>
2019-04-08 13:51:10 -0400

    added image recognition code but other stuff isn't pushed. Need to fit into code

--- catkin_ws/src/mie443_contest3/src/contest3.cpp
+++ catkin_ws/src/mie443_contest3/src/contest3.cpp
@@ -3,6 +3,17 @@
 #include <imageTransporter.hpp>
 #include <kobuki_msgs/BumperEvent.h>
 #include <time.h>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/core/types_c.h"
+#include "opencv2/imgproc.hpp"
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
@@ -196,4 +207,119 @@ int main(int argc, char **argv)
 	}
 
 	return 0;
+}
+                     
+// Image Detection Code, put wherever necessary
+{
+    int matchFlag = 0;
+    cv::Mat sceneImage = imageTransporter.getImg();
+    cv::Mat plant = imread("/path/to/image.jpg", IMREAD_GRAYSCALE);
+    
+    int minHessian = 400;
+    Ptr<SURF> detector = SURF::create(minHessian);
+    vector<KeyPoint> keypointsSceneImage, keypointsPlant;
+    detector->detect(sceneImage, keypointsSceneImage);
+    detector->detect(plant, keypointsPlant);
+    
+    Ptr<SURF> extractor = SURF::create();
+    Mat descriptorSceneImage;
+    Mat descriptorPlant;
+    extractor->compute(sceneImage, keypointsSceneImage, descriptorSceneImage);
+    extractor->compute(plant, keypointsPlant, descriptorPlant);
+    
+    int notEnoughMatches = 0;
+    FlannBasedMatcher matcher;
+    std::vector<DMatch> matches;
+    matcher.match(descriptorPlant, descriptorSceneImage, matches);
+    
+    double max_dist = 0; double min_dist = 100;
+    for( int i = 0; i < descriptorPlant.rows; i++ ){
+        double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+    std::vector<DMatch>good_matches;
+    
+    for(int i = 0; i < descriptorPlant.rows; i++){
+        if( matches[i].distance <= max(2*min_dist, 0.02) ){
+            good_matches.push_back( matches[i]);
+        }
+    }
+    Mat img_matches;
+    drawMatches(plant, keypointsPlant, sceneImage, keypointsSceneImage,
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    
+    if(good_matches.size() < 10) notEnoughMatches = 1;
+    std::vector<Point2f>obj;
+    std::vector<Point2f>scene;
+
+    int HMDFlag = 0; int CNFlag = 0;
+    int rows; int cols;
+    double conditionNumber;
+    Mat homographyMatrix, singularValues, U, Vt = Mat();
+    std::vector<Point2f> obj_corners(4);
+    std::vector<Point2f> scene_corners(4);
+    Size s;
+    
+    if(!notEnoughMatches){
+        
+        for(int i = 0; i<good_matches.size(); i++){
+            //-- Get the keypoints from the good matches
+            obj.push_back(keypointsPlant[good_matches[i].queryIdx].pt);
+            scene.push_back(keypointsSceneImage[good_matches[i].trainIdx].pt);
+        }
+        
+        //-- Get homography matrix
+        homographyMatrix = findHomography(obj,scene,RANSAC);
+        
+        //-- Get the corners from the image_1 ( the object to be "detected" )
+        obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint(plant.cols, 0);
+        obj_corners[2] = cvPoint(plant.cols, plant.rows); obj_corners[3] = cvPoint(0, plant.rows);
+        
+        perspectiveTransform( obj_corners, scene_corners, homographyMatrix);
+        
+        //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+        line( img_matches, scene_corners[0] + Point2f( plant.cols, 0), scene_corners[1] + Point2f( plant.cols, 0), Scalar(0, 255, 0), 4 );
+        line( img_matches, scene_corners[1] + Point2f( plant.cols, 0), scene_corners[2] + Point2f( plant.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[2] + Point2f( plant.cols, 0), scene_corners[3] + Point2f( plant.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[3] + Point2f( plant.cols, 0), scene_corners[0] + Point2f( plant.cols, 0), Scalar( 0, 255, 0), 4 );
+        
+        //-- Show detected matches
+        //imshow( "Good Matches & Homography Calculation", img_matches );
+        
+        for( int i = 0; i < (int)good_matches.size(); i++ ){
+            //printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx );
+        }
+        
+        std::cout << "Homography Matrix = " << std::endl << " " << homographyMatrix << std::endl;
+        
+        //-- Check determinant of the matrix to see if its too close to zero
+        double HMDeterminant = determinant(homographyMatrix);
+        if (HMDeterminant>0.1) HMDFlag = 1;
+        else HMDFlag = 0;
+        std::cout << "Determinant of matrix is: " << HMDeterminant << std::endl;
+        
+        //-- DO SVD on homography matrix and check its values
+        singularValues = Mat();
+        U, Vt = Mat();
+        CNFlag = 0;
+        SVDecomp(homographyMatrix, singularValues, U, Vt, 2);
+        
+        std::cout << "Printing the singular values of the homography matrix: " << std::endl;
+        s = singularValues.size();
+        rows = s.height;
+        cols = s.width;
+        for (int i = 0; i < rows; i++)
+            for(int j = 0; j < cols; j++){
+                std::cout << "Element at " << i << " and " << j << " is " << singularValues.at<double>(i,j) << std::endl;
+            }
+        conditionNumber = singularValues.at<double>(0,0)/singularValues.at<double>(2,0);
+        std::cout << "Condition number is: " << conditionNumber << std::endl;
+        if(conditionNumber <= 10000000){
+            std::cout << "Condition number check passed" << std::endl;
+            CNFlag = 1;
+        }
+    }
+    if(CNFlag == 1 && HMDFlag == 1) matchFlag = 1;
 }
